3dbc0333f8437063f864cdbe2b530747b48105c6
dschaad <97546064+dschaad@users.noreply.github.com>
2023-01-16 13:46:33 -0700

    created CandyCounter and script

---
+++ Assets/Scripts/CandyCounter.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CandyCounter : MonoBehaviour
+{
+    public int candy; // Might want to make this long in case of integer overflow, but int should suffice
+    public TextMeshProUGUI candyText;
+    
+    public static CandyCounter instance;
+
+    void Awake()
+    {
+        instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        candy = 0;
+        // Calls AddCandy() after 0 seconds, every 1 second
+        InvokeRepeating("AddCandy", 0.0f, 1.0f);
+    }
+
+    void AddCandy()
+    {
+        candy += 1;
+    }
+
+    public int GetCandy()
+    {
+        return candy;
+    }
+    
+    public void ResetCandy()
+    {
+        candy = 0;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        // Changes the text to Candies: <candies>
+        candyText.text = "Candies: " + candy;
+    }
+}
