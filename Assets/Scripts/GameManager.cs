780f3dbdbb92879705f6d9f257d4f23c467b4443
dschaad <97546064+dschaad@users.noreply.github.com>
2023-01-26 10:11:30 -0700

    Create GameManager.cs
    Working with MainManager created problems for the feature bar, so created a new script for the time being

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject EatButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Hide the eat candies button at the start
+        EatButton.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Reveal the eat candies button after getting 10 candies
+        if (CandyCounter.instance.GetCandy() >= 10)
+        {
+            EatButton.SetActive(true);
+        }
+    }
+}
