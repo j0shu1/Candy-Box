f9a008d3efd77b9340fee7b8a9d66c03ea05bc93
dschaad <97546064+dschaad@users.noreply.github.com>
2023-02-02 17:08:56 -0700

    GameManager replaces CandyCounter
    GameManager now handles CandyCounter functions, including tracking candy total. GameManager now persists across scenes with no data lost.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,28 +1,58 @@
 using UnityEngine;
 
+/* 
+This script will run across all scenes. As such, please only include code that will exist for every scene. 
+Any code that doesn't should simply reference the GameManager object. (e.g. GameManager.Instance.GetCandy())
+*/
+
 public class GameManager : MonoBehaviour
 {
-    public GameObject EatButton;
+    public static GameManager Instance;
     public GameObject addFeatureButton;
     public GameObject featureBar;
+    
+    public int candy; // Might want to make this long in case of integer overflow, but int should suffice
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
     
     // Start is called before the first frame update
     void Start()
     {
-        // Hide the eat candies button at the start
-        EatButton.SetActive(false);
         addFeatureButton.SetActive(false);
+
+        candy = 0;
+        // Calls AddCandy() after 0 seconds, every 1 second
+        InvokeRepeating("AddCandy", 0.0f, 1.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Reveal the eat candies button after getting 10 candies
-        if (CandyCounter.instance.GetCandy() >= 10)
-        {
-            EatButton.SetActive(true);
-        }
-        if (CandyCounter.instance.GetCandy() >= 30)
+        if (GetCandy() >= 30)
             addFeatureButton.SetActive(true);
+    }
+
+    void AddCandy()
+    {
+        candy += 1;
+    }
+
+    public int GetCandy()
+    {
+        return candy;
+    }
+    
+    public void ResetCandy()
+    {
+        candy = 0;
     }
 }
