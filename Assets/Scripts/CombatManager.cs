ba0de75996812f8a228fb25cfddf794650477241
dschaad <97546064+dschaad@users.noreply.github.com>
2023-02-13 20:28:12 -0700

    Progress on Combat Demo

--- Assets/Scripts/CombatManager.cs
+++ Assets/Scripts/CombatManager.cs
@@ -5,18 +5,16 @@ using UnityEngine;
 public class CombatManager : MonoBehaviour
 {
     public GameObject player;
-    public GameObject enemy;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         player.GetComponent<Rigidbody2D>().velocity = new Vector2(30, 0);
-        enemy.GetComponent<Rigidbody2D>().velocity = new Vector2(-30, 0);
     }
 }
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Enemy : MonoBehaviour
+{
+    public float currentHP;
+    public float maxHP;
+    public Image HealthBarFill;
+    public int attack;
+    
+    
+    void Start()
+    {
+        attack = GameManager.Instance.GetAttack();
+    }
+
+    void Update()
+    {
+        // Move to the left
+        GetComponent<Rigidbody2D>().velocity = new Vector2(-30, 0);
+    }
+    
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.ToString() == "Player (UnityEngine.GameObject)")
+        {
+           InvokeRepeating("Damage", 0.0f, 1.0f);
+        }
+    }
+
+    void Damage()
+    {
+        currentHP -= attack;
+        HealthBarFill.fillAmount = currentHP / maxHP;
+        if (currentHP == 0)
+        {
+            Destroy(gameObject);
+        }
+    }    
+}
