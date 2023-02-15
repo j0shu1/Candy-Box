68f5113b9a5af0dcb079df10e8bc8fb5c31eeac2
chanyoung1999 <93686210+chanyoung1999@users.noreply.github.com>
2023-02-15 16:30:50 -0700

    Added Healthbar Scripts

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+public class HealthBar : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+
+    void Start() 
+    {
+        currentHealth = maxHealth;
+    }
+
+    void Update() 
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(1);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+
+    }
+}
