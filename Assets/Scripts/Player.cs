bf973f8b5a2f2c93f8c289728becefebf983e62d
dschaad <97546064+dschaad@users.noreply.github.com>
2023-02-22 11:20:24 -0700

    Moved Health Bar scripts to Player
    Moved Health Bar variables and functions into Player script. Doesn't quite work with scene changes. Also removed the HealthBar script from the HealthBar object, only in CandyBox though.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,24 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
+    public int maxHealth;
+    public int currentHealth;
+    public GameObject healthBar;
+    public Slider healthSlider;
+    
     private int attack = 10; // Just a default value
     private int weaponSelection = 0; //default weapon selected
     
     // Start is called before the first frame update
     void Start()
     {
+        // healthBar = GameObject.FindGameObjectWithTag("HealthBar");
+        healthSlider = healthBar.GetComponent<Slider>();
 
+        maxHealth = 100;
+        currentHealth = maxHealth;
+        InvokeRepeating("Regen", 0.0f, 1.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        healthSlider.value = currentHealth;
 
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(10);
+        }
+        if (currentHealth < 0)
+        {
+            currentHealth = 0;
+        }
+    }
 
 
     // COMBAT FUNCTIONS
@@ -29,6 +48,19 @@ public class Player : MonoBehaviour
     public int GetAttack()
     {
         return attack;
+    }
+
+    void Regen()
+    {
+        if (currentHealth < maxHealth)
+        {
+            currentHealth += 1;
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
     }
 
     // WEAPON SELECTION
