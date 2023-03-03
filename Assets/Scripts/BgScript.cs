435d49dae387c631d9e59a143e5417a41989748c
shumwaynate <nateshumway1@gmail.com>
2023-03-02 17:11:27 -0700

    Finished sound logics

--- Assets/Scripts/BgScript.cs
+++ Assets/Scripts/BgScript.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BgScript : MonoBehaviour
 {
     // Start is called before the first frame update
     public static BgScript BgInstance;
+    public AudioSource Audio; //Background audio source
+    public AudioClip[] MusicClips; //array of music
+    //item 0: Main ; item 1: Fight
 
     private void Awake()
     {
--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -6,7 +6,30 @@ using UnityEngine.SceneManagement;
 public class ChangeScene : MonoBehaviour
 {
     public void MoveToScene(int sceneID){
+        //Switch Scene
         SceneManager.LoadScene(sceneID);
+        //Make click sound when switching scenes
         SfxManager.sfxInstance.Audio.PlayOneShot(SfxManager.sfxInstance.Click);
+
+        //Switch the background music
+        AudioSource source = new AudioSource(); 
+        if(BgScript.BgInstance.Audio.clip == BgScript.BgInstance.MusicClips[0]) //detects if current music is wrong
+        {
+            if(sceneID == 5) //if the scene is switched to attack scene
+            {            //stop music, set new music, play music
+                BgScript.BgInstance.Audio.Stop(); 
+                BgScript.BgInstance.Audio.clip = BgScript.BgInstance.MusicClips[1];
+                BgScript.BgInstance.Audio.Play(0);
+            }
+        }
+        if(BgScript.BgInstance.Audio.clip == BgScript.BgInstance.MusicClips[1]) //detects if current music is wrong
+        {
+            if(sceneID != 5)//if the scene is switched from attack scene
+            {           //stop music, set new music, play music
+                BgScript.BgInstance.Audio.Stop();
+                BgScript.BgInstance.Audio.clip = BgScript.BgInstance.MusicClips[0];
+                BgScript.BgInstance.Audio.Play(0);
+            }
+        }
     }
 }
